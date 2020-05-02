510c6fe2f6e92941d49adf3fe49757b5d5201295
William <liamshere98@gmail.com>
2020-05-02 12:33:49 +0300

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance = null;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    public GameObject introBackground;
+    public void StartGame()
+    {
+        introBackground.SetActive(false);
+    }
+
+    public void WinGame()
+    {
+
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void LoseGame()
+    {
+
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
