6170e43ab00e8c339882677de6f9993235ddeb2b
William AbouSharaf <liams_1998@hotmail.com>
2020-05-03 00:58:39 +0300

    added camera functionality

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Camera cam;
+    private float targetZoom;
+    private float zoomFactor = 3f;
+    private float zoomLerpSpeed = 10;
+    private float originalCamZooom;
+
+    public float minZoom;
+    public float maxZoom;
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        cam.orthographicSize = minZoom;
+    }
+
+    private bool zoomedOut = false;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Debug.Log("clicked shit");
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, maxZoom, Time.deltaTime * zoomLerpSpeed);
+        }
+        else
+        {
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, minZoom, Time.deltaTime * zoomLerpSpeed);
+        }
+    }
+}
