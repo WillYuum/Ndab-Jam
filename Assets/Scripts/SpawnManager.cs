e2ade49f3b0cf4c8896812144a78bf5c5e789cba
William AbouSharaf <liams_1998@hotmail.com>
2020-05-03 19:23:13 +0300

    added citizen prefabs + fixes in scene

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    public Transform[] spawnPnts;
+    public GameObject[] citizens; 
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void SpawnEnemies()
+    {
+        if (GameManager.instance.gameIsOn)
+        {
+            
+        }
+    }
+}
