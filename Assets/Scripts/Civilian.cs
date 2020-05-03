d4f14355c1321e95fbef236edf3b4d210c183af9
William AbouSharaf <liams_1998@hotmail.com>
2020-05-03 20:45:06 +0300

    change houses style in scene

--- Assets/Scripts/Civilian.cs
+++ Assets/Scripts/Civilian.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Civilian : MonoBehaviour
 {
 
-    public GameObject Player;
+    //public GameObject Player;
 
     private MomentumManager momentumBar;
     public float amountOfMomentumOnHit = 0.1f;
@@ -17,7 +17,7 @@ public class Civilian : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.CompareTag("House") && Player.GetComponent<Player>().isHoldingCiv == false)
+        if (collision.collider.CompareTag("House") && GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().isHoldingCiv == false)
         {
             Destroy(gameObject);
             momentumBar.IncreaseMomentum(amountOfMomentumOnHit);
