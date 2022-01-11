abec7afadfafbf233c66c5c3abc40d4a151688f6
WilliamAB <liams_1998@hotmail.com>
2022-01-11 07:32:27 +0200

    Major update in game UI

---
+++ Assets/Scripts/NodeStabilizer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeStabilizer : MonoBehaviour
+{
+    [SerializeField] private GameObject[] nodeToEnable;
+    [SerializeField] private GameObject[] nodeToDisable;
+
+
+    private void Awake()
+    {
+        foreach (GameObject node in nodeToEnable)
+        {
+            node.SetActive(true);
+        }
+
+        foreach (GameObject node in nodeToDisable)
+        {
+            node.SetActive(false);
+        }
+    }
+}
