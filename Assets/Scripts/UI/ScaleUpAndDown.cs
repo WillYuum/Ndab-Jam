d363b282ddd060948bb00ea5be9d0b0bd45b4a39
WilliamAB <liams_1998@hotmail.com>
2022-01-11 06:50:15 +0200

    Add scale up and down script

---
+++ Assets/Scripts/UI/ScaleUpAndDown.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ScaleUpAndDown : MonoBehaviour
+{
+    [Header("Scale properties")]
+    [Range(0, 1)] [SerializeField] private float maxScale = 1;
+    [Range(0, 1)] [SerializeField] private float minScale = 0;
+    [SerializeField] private float scaleDuration = 1;
+
+
+
+    [Header("Settings")]
+    [SerializeField] private bool scaleOnStart = false;
+    [SerializeField] private bool loop = false;
+    [SerializeField] private Ease easeType;
+
+
+    private Vector3 defaultScale;
+
+    void Start()
+    {
+        defaultScale = transform.localScale;
+        if (scaleOnStart)
+        {
+            StartScale();
+        }
+    }
+
+
+    public void StartScale()
+    {
+        ScaleUp();
+    }
+
+    private void ScaleUp()
+    {
+        if (loop == false) return;
+
+        transform
+        .DOScale(defaultScale + (defaultScale * maxScale), scaleDuration)
+        .SetEase(easeType)
+        .OnComplete(ScaleDown);
+    }
+
+    private void ScaleDown()
+    {
+        if (loop == false) return;
+
+        transform
+        .DOScale(defaultScale + (defaultScale * minScale), scaleDuration)
+        .SetEase(easeType)
+        .OnComplete(ScaleUp);
+    }
+
+}
