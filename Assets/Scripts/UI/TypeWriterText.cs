a493117cc4fb49278eca0dfa4413073ad4a9ebb8
WilliamAB <liams_1998@hotmail.com>
2022-01-11 06:50:07 +0200

    Add typewrite script

---
+++ Assets/Scripts/UI/TypeWriterText.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class TypeWriterText : MonoBehaviour
+{
+    [SerializeField] private bool showTextOnLoad = false;
+
+    [SerializeField] private float delay = .1f;
+    private string fullText;
+    private string currentText = "";
+
+    void Start()
+    {
+        if (showTextOnLoad)
+        {
+            StartShowText();
+        }
+    }
+
+    public void StartShowText()
+    {
+        StartCoroutine(ShowText());
+    }
+
+
+    IEnumerator ShowText()
+    {
+        TextMeshProUGUI tmpro = GetComponent<TextMeshProUGUI>();
+        fullText = tmpro.text;
+        tmpro.text = "";
+        for (int i = 0; i < fullText.Length; i++)
+        {
+            currentText = fullText.Substring(0, i);
+            tmpro.text = currentText;
+            yield return new WaitForSeconds(delay);
+        }
+    }
+}
